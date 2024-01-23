6111f58d34f6a473264210ff7447a81882b021a4
ademsuslu <ademsuslu9080@gmail.com>
2024-01-23 03:47:18 +0300

    Conditions

--- app/components/data/Data.js
+++ app/components/data/Data.js
@@ -46,3 +46,82 @@ export const BlogData = [
     url: "/",
   },
 ];
+
+export const kiralamaKosullari = [
+  {
+    baslik: "Kiralama Süresi:",
+    liste: [
+      "Minimum kiralama süresi nedir?",
+      "Maksimum kiralama süresi nedir?",
+      "Günlük, haftalık veya aylık kiralama seçenekleri mevcut mu?",
+    ],
+  },
+  {
+    baslik: "Ücretlendirme ve Ödemeler:",
+    liste: [
+      "Günlük, haftalık veya aylık kira ücretleri nedir?",
+      "Ek ücretler (sigorta, vergiler, havaalanı vergisi vb.) nelerdir?",
+      "Depozito miktarı nedir?",
+      "İptal politikası nedir?",
+    ],
+  },
+  {
+    baslik: "Sigorta ve Güvence:",
+    liste: [
+      "Temel sigorta kapsamı neleri içerir?",
+      "Ekstra sigorta seçenekleri mevcut mu?",
+      "Kasko kapsamı nedir?",
+    ],
+  },
+  {
+    baslik: "Kiralanacak Araç Kategorileri:",
+    liste: [
+      "Hangi tür araçlar kiralama için mevcut?",
+      "Araç modelleri ve özellikleri nelerdir?",
+    ],
+  },
+  {
+    baslik: "Yol Koşulları:",
+    liste: [
+      "Kiralanan aracın kullanımı için belirlenen coğrafi sınırlamalar var mı?",
+      "Yurt dışına çıkma izni var mı?",
+    ],
+  },
+  {
+    baslik: "Yakıt Politikası:",
+    liste: [
+      "Kiralama ücretine yakıt dahil mi?",
+      "Araçla teslim edilen yakıt seviyesi nedir?",
+      "Yakıtı doldurmadan iade etme seçeneği var mı?",
+    ],
+  },
+  {
+    baslik: "Teslimat ve İade Koşulları:",
+    liste: [
+      "Araç nereden teslim alınabilir?",
+      "Araç nereye iade edilebilir?",
+      "Teslimat ve iade saatleri nedir?",
+    ],
+  },
+  {
+    baslik: "Sorumluluklar ve Kısıtlamalar:",
+    liste: [
+      "Kullanıcıların araçla ilgili sorumlulukları nelerdir?",
+      "Kullanım kısıtlamaları var mı?",
+    ],
+  },
+  {
+    baslik: "Araç Durumu ve Bakım:",
+    liste: [
+      "Araç teslim alınmadan önce ve iade edilmeden önce yapılan kontrol ve bakımlar nelerdir?",
+      "Kullanıcıların araçtaki hasarı bildirme prosedürü nedir?",
+    ],
+  },
+  {
+    baslik: "Müşteri Desteği:",
+    liste: [
+      "Müşteri destek hizmetleri nelerdir?",
+      "Acil durum durumlarında müşterilere yardım sağlama politikası nedir?",
+    ],
+  },
+];
--- app/conditions/page.js
+++ app/conditions/page.js
@@ -1,3 +1,43 @@
-export default function Page() {
-  return <div>Hello, Conditions Page!</div>;
-}
+"use client";
+import Image from "next/image";
+import { kiralamaKosullari } from "../components/data/Data";
+import React from "react";
+import { Container, Row, Col, ListGroup } from "react-bootstrap";
+
+const KiralamaKosullari = () => {
+  return (
+    <Container>
+      {kiralamaKosullari.map((kosul, index) => (
+        <Row key={index} className="my-4">
+          <Col>
+            <ListGroup variant="flush" className="bg-transparent border-0">
+              <ListGroup.Item
+                className="bg-transparent border-0 "
+                variant="secondary"
+              >
+                <h4> {kosul.baslik}</h4>
+              </ListGroup.Item>
+              {kosul.liste.map((madde, idx) => (
+                <ListGroup.Item key={idx} className="bg-transparent border-0 ">
+                  {madde}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col>
+            <Image
+              width={150}
+              height={150}
+              className="rounded cursor-pointer"
+              src="https://images.pexels.com/photos/909907/pexels-photo-909907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Car"
+            />
+            Araba seçenekleri gösterilicek
+          </Col>
+        </Row>
+      ))}
+    </Container>
+  );
+};
+
+export default KiralamaKosullari;
