1d772bbd5cd4bf2d2925b19bcb78c6a7aec77f92
ademsuslu <ademsuslu9080@gmail.com>
2024-01-26 01:20:48 +0300

    cors

--- app/hooks/getAllCar.js
+++ app/hooks/getAllCar.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const RqCarFetch = () => {
-  return axios.get("http://localhost:3000/api/car");
+  return axios.get(
+    "https://cors-anywhere.herokuapp.com/http://localhost:3000/api/car"
+  );
 };
 export const useCarData = () => {
   return useQuery("rqsuper-heros", RqCarFetch);
